c097dd1dcac3aafc81e5691c472d38fdf899b3b4
Michael Welch <michaelgwelch@gmail.com>
2016-11-19 06:49:37 -0600

    Initial commit

---
+++ BusDepotTest2/ContentImage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BusDepotTest2
+{
+	public class ContentImage
+	{
+		public String ProductNum { get; set; }
+		public string ImageUrl { get; set; }
+		public string Caption { get; set; }
+		public string System { get; set; }
+		public string Identifier { get; set; }
+	}
+}
---
+++ BusDepotTest2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MassTransit;
+
+namespace BusDepotTest2
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var bus = GetServiceBus();
+
+			for (int i = 0; i < 100; i++)
+			{
+				bus.Publish(new ContentImage
+				{
+					ProductNum = "6789934",
+					ImageUrl = "myurl" + i,
+					System = "mysystem",
+					Caption = "my caption",
+					Identifier = i.ToString()
+				});
+			}
+
+			Console.WriteLine("Done");
+
+			bus.Dispose();
+		}
+
+
+		private static IServiceBus GetServiceBus()
+		{
+			return ServiceBusFactory.New(sb =>
+			{
+				sb.UseRabbitMq(r => r.ConfigureHost(new Uri("rabbitmq://localhost/images/completed_images"), h =>
+				{
+					h.SetUsername("guest");
+					h.SetPassword("guest");
+				}));
+				sb.UseJsonSerializer();
+				sb.ReceiveFrom(new Uri("rabbitmq://localhost/images/completed_images"));
+			});
+
+		}
+	}
+
+	
+}
